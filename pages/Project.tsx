42df7755510a27fdf5151ee51c05237a738a3bd7
Anuj Shaan <anujprakashshaan@gmail.com>
2023-01-28 00:18:11 +0530

    first commit

--- pages/Project.tsx
+++ pages/Project.tsx
@@ -1,5 +1,10 @@
 import {motion as m} from 'framer-motion'
 
+import  ProjectCard  from "../components/ProjectCard"
+import ProjectData from '../projectData.json'
+
+
+
 const Project = () => {
   return (
     <m.div
@@ -7,8 +12,16 @@ const Project = () => {
       exit={{opacity:1}}
       initial={{y:"100%"}}
       transition={{duration:0.75 , ease:"easeInOut"}}
-     className="bg-[#aeb1b8] h-screen w-full absolute top-0">
-
+     className="bg-[#a3a3da] h-screen w-full absolute top-0">
+     <div  className="max-w-6xl mx-auto space-x-16 h-[86vh] flex justify-center">
+        {
+          ProjectData.map((data)=>(
+            <ProjectCard key={data.id}  data={data} />
+          ))
+        }
+     </div>
+        {/* <button className="absolute bottom-8 left-[50%] translate-x-[-50%] ">Show more</button> */}
+    
     </m.div>
   )
 }
